887ec44151103fa8a03c3205fb36b703921afac1
oyesido <yusufoladele94@gmail.com>
2023-02-17 02:54:10 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-/**
+/** 
  * main - main block
  * return: 0
  */
-int main(void)
+ int main(void)
 {
 	puts("\"programming is like develop a multilingual puzzle");
 	return (0);
-
 }
-
-

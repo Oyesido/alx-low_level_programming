8e23f60e8f33907cc95b1d6a11b25ddc86f55ef0
oyesido <yusufoladele94@gmail.com>
2023-02-17 08:49:04 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- *
- * main  = A proprogrammthat prints new line with puts function
+  * main  - A proprogrammthat prints new line with puts function
  * return: 0 (success)
- *
- ** /
- 
+ */
 int main(void)
 {
-puts{"\n"programming is like building a multilingual puzzle");
-return (0);
+puts("\n"programming is like building a multilingual puzzle");
+return(0);
 }
+

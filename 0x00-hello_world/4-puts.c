ea6c7bc5cec94561ed63dcb8189a4737ed5be761
oyesido <yusufoladele94@gmail.com>
2023-02-18 03:14:17 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
-
-
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+

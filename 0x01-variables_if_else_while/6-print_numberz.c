2480bf16b6af0b7ffeed285e1e292701b4246b6f
oyesido <yusufoladele94@gmail.com>
2023-02-19 06:38:06 -0800

    6 print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,16 +10,12 @@
  */
 int main(void)
 {
-	putchar('0');
-	putchar('1');
-	putchar('2');
-	putchar('3');
-	putchar('4');
-	putchar('5');
-	putchar('6');
-	putchar('7');
-	putchar('8');
-	putchar('9');
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+	putchar(n);
+	}
 	putchar('\n');
 	return (0);
 }

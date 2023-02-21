d569de5dd454ef0e59859d76afeec2ccab08c624
oyesido <yusufoladele94@gmail.com>
2023-02-21 08:45:31 -0800

    0 putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,31 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function is the entry point of the program.
+ *              It prints the string "_putchar" to the console
+ *              using the _putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/* Call the _putchar function to print each char string "_putchar" */
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 
-	int main(void)
-	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-	}
+/* Print a newline character to the console */
+_putchar('\n');
+
+/* Indicate that the program has completed successfully by returning 0 */
+return (0);
+}
+
 
